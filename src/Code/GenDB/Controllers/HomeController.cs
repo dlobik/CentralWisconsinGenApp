78f46955d3c56421b40d954599a9d6d5a4130444
Rudolph Dlobik <rdlob572@uwsp.edu>
2018-04-23 16:20:50 -0500

    Created a Search Parameters ViewModel to pass search parameters to "Search" method in controller. Next step is figuring out how to create queries in controller and populate each view with returned results from queries. -rd

--- src/Code/GenDB/Controllers/HomeController.cs
+++ src/Code/GenDB/Controllers/HomeController.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GenDB.Models;
+using GenDB.ViewModels;
 
 namespace GenDB.Controllers
 {
     public class HomeController : Controller
     {
+        private GenContext db = new GenContext();
 
         public ActionResult Index()
         {
@@ -29,7 +32,47 @@ namespace GenDB.Controllers
             return View();
         }
 
-        
+        /* We can pass all of the search parameters here through the viewmodel SearchParameters.
+         * Once we have them in this action, we can create DB queries based on them for each table.
+         * After we create queries in this Search method, How do we pass into each individual View / Model??? (Obit, Nat, Census)
+         */
+        public ActionResult Search(SearchParameters searchParam)
+        {
+            var fName = searchParam.FirstName;
+            var lName = searchParam.LastName;
+            var altName = searchParam.AltName;
+            var recordDate = searchParam.DateOfRecord;
+            var county = searchParam.County;
 
+
+
+            return View();
+        }
+
+        /* Test JsonResult Method
+        public JsonResult GetSearchingData(string SearchBy, string SearchValue)
+        {
+            List<Obit> Obits = new List<Obit>();
+            if (SearchBy == "fname")
+            {
+                try
+                {
+                    string fname = SearchValue;
+                    Obits = db.Obit.Where(x => x.FirstName == fname || SearchValue == null).ToList();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("{0} Is not a First Name in DB", SearchValue);
+                }
+                return Json(Obits, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                Obits = db.Obit.Where(x => x.LastName == SearchValue || SearchValue == null).ToList();
+                return Json(Obits, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+        */
     }
 }
---
+++ src/Code/GenDB/ViewModels/SearchParameters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GenDB.ViewModels
+{
+    public class SearchParameters
+    {
+        [DisplayName("First Name:  ")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name:  ")]
+        public string LastName { get; set; }
+        [DisplayName("Alternate Name:  ")]
+        public string AltName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayName("Date of Record:  ")]
+        public DateTime DateOfRecord { get; set; }
+        [DisplayName("County:  ")]
+        public string County { get; set; }
+    }
+}
