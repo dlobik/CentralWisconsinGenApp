08c52fc1350b8b074c05ce9899b560a1061f049e
Rudolph Dlobik <rdlob572@uwsp.edu>
2018-05-09 13:06:18 -0500

    Added: Page field to CensusMember Repo

--- src/Code/GenDB/Models/CensusMember.cs
+++ src/Code/GenDB/Models/CensusMember.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,9 @@ namespace GenDB.Models
     public class CensusMember : Model
   {
         public int ID { get; set; }
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
         public int Age { get; set; }
         public string Page { get; set; }
